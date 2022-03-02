13e070e00f354f2eda127edbf5f021355ded1982
KaishAli <360mohdkaishali.cs@gmail.com>
2022-03-02 14:47:02 +0530

    Updates

---
+++ precache-manifest.4408da60c43e106f77edd3167600ba57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92bf0b9e6f19f70f0294",
+    "url": "/React-Coloer-Picker/static/css/main.44d3c808.chunk.css"
+  },
+  {
+    "revision": "92bf0b9e6f19f70f0294",
+    "url": "/React-Coloer-Picker/static/js/main.92bf0b9e.chunk.js"
+  },
+  {
+    "revision": "077172c37b730ab87fde",
+    "url": "/React-Coloer-Picker/static/css/1.0083a361.chunk.css"
+  },
+  {
+    "revision": "077172c37b730ab87fde",
+    "url": "/React-Coloer-Picker/static/js/1.077172c3.chunk.js"
+  },
+  {
+    "revision": "26c98d37918a3e82fd52",
+    "url": "/React-Coloer-Picker/static/js/runtime~main.26c98d37.js"
+  },
+  {
+    "revision": "cdeb3d44cebdbf89eea5d7584aea780a",
+    "url": "/React-Coloer-Picker/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-Coloer-Picker/precache-manifest.4408da60c43e106f77edd3167600ba57.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-Coloer-Picker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
